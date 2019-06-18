1e97c384b73cb50ee2a53a6a05d67f763aca7f51
CS <Cara@MBP15in2015.local>
2019-06-17 21:10:08 -0400

    30 second timer is set up in JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,29 @@
-
-
-
 // Function to hide start button
 $("#start").click(function(){
     $("#start").hide();
 })
+
+// Array of questions and answers
+var questions = {
+    "What is the first color in the rainbow?": ["Red", "Orange", "Yellow", "Green"]
+};
+
+
+
+// Here are the variables to set up the timer
+var timeLeft = 30;
+var elem = document.getElementById("timer");
+var timerId = setInterval(countdown, 1000);
+// Here is the function for the timer
+function countdown() {
+    if (timeLeft == -1) {
+        clearTimeout(timerId);
+        doSomething();
+    } else {
+        elem.innerHTML = "Time remaining: " + timeLeft + " seconds";
+        timeLeft--;
+    }
+}
+// Here is the function for what will happen when the timer runs out
+function doSomething();
+// This is where I can have it show the answer if time runs out
