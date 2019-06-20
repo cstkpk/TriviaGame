b2f838aa4fd879717d6a35971cf911b7e15a41e8
CS <Cara@MBP15in2015.local>
2019-06-20 19:52:47 -0400

    Cleaned up code and added comments for clarity

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-// When the page loads:
+// When the page loads, we make sure everything is hidden except for the start button:
 $(document).ready(function() {
     $("#timer").hide();
     $("#question").hide();
@@ -60,45 +60,45 @@ var questionArr = [
     }
 ]
 
-// Array for winning images
+// Array of images to display if the quesiton was answered correctly
 var winImage = ["assets/images/celebration1.gif", "assets/images/celebration2.gif", "assets/images/celebration3.gif", "assets/images/celebration4.gif", "assets/images/celebration5.gif", "assets/images/celebration6.gif", "assets/images/celebration7.gif", "assets/images/celebration8.gif", "assets/images/celebration9.gif", "assets/images/celebration10.gif"];
 winImageCount = 0;
 
-// Array for losing images
+// Array of images to display if the question was answered incorrectly (or time ran out)
 var loseImage = ["assets/images/loss1.gif", "assets/images/loss2.gif", "assets/images/loss3.gif", "assets/images/loss4.gif", "assets/images/loss5.gif", "assets/images/loss6.gif", "assets/images/loss7.gif", "assets/images/loss8.gif", "assets/images/loss9.gif", "assets/images/loss10.gif"];
 loseImageCount = 0;
 
-// This function will display an image if the user wins the round
+// This function will display an image if the user wins the round, and the counter for winImage is updated
 function displayWinImage() {
     $("#image-holder").html("<img src=" + winImage[winImageCount] + " width='400px'>");
     winImageCount++;
-    console.log("Image: " + winImageCount);
 }
 
-// This function will display an image if the user loses the round
+// This function will display an image if the user loses the round, and the counter for loseImage is updated
 function displayLoseImage() {
     $("#image-holder").html("<img src=" + loseImage[loseImageCount] + " width='400px'>");
     loseImageCount++;
 }
 
-// Variables for stats
+// Variables for stats; all stats start at 0
 var wins = 0;
 var losses = 0;
 var unanswered = 0;
 
-
+// Functions to update stats
 function updateWins() {
-    $("#wins").text("Wins: " + wins);
+    $("#wins").text("Correct: " + wins);
 }
 
 function updateLosses() {
-    $("#losses").text("Losses: " + losses);
+    $("#losses").text("Incorrect: " + losses);
 }
 
 function updateUnanswered() {
     $("#unanswered").text("Unanswered: " + unanswered);
 }
 
+// Function to display stats (called at the end of the game)
 function showStats() {
     $("#wins").show();
     $("#losses").show();
@@ -108,44 +108,36 @@ function showStats() {
     updateUnanswered();
 }
 
-
+// Counter variable for cycling through the questions and answers array
 var counter = 0;
 console.log("Correct answer: " + questionArr[counter].answerCorrect);
 
 
-
+// Variables that need outer scope for the timer
 var timerId;
 var timeLeft;
 
 // Here is the function to start the timer
 var startTimer = function() {
     // Here are the variables for the timer
-    timeLeft = 30;
+    timeLeft = 5;
     timerId = setInterval(countdown, 1000);
-    // var elem = document.getElementById("timer");
-    // var elem = $("#timer");
     // Here is the function for the timer
     $("#timer").text("Time remaining: " + timeLeft + " seconds");
-    // elem.innerHTML = "Time remaining: " + timeLeft + " seconds";
     function countdown() {
         if (timeLeft == -1) {
             clearTimeout(timerId);
-            // answerPage();
             outOfTime();
             unanswered++;
         } else {
-            // elem.innerHTML = "Time remaining: " + timeLeft + " seconds";
             $("#timer").text("Time remaining: " + timeLeft + " seconds");
             timeLeft--;
         };
     } 
 }
 
-
-
-// Function to clear the page and show the correct answer
+// Function to hide the previous question/answers and show the correct answer (if the question was answered incorrectly)
 function answerPage() {
-    // $("#timer").hide();
     $("#question").hide();
     $(".hidebutton").hide();
     $("#yay").show();
@@ -154,9 +146,11 @@ function answerPage() {
     setTimeout(newQuestion, 3000);
     $("#image-holder").show();
     displayLoseImage();
+    $("#reveal").show();
+    $("#reveal").text("The correct answer was " + questionArr[counter].answerCorrect);
 }
 
-// Function to clear the page and say congratulations
+// Function to hide the previous question/answers and show celebratory elements (if the question was answered correctly)
 function congrats() {
     // $("#timer").hide();
     clearTimeout(timerId);
@@ -169,6 +163,7 @@ function congrats() {
     displayWinImage();
 }
 
+// Function to hide the previous question/answers and show the correct answer (if the question was not answered in time)
 function outOfTime() {
     $("#question").hide();
     $(".hidebutton").hide();
@@ -178,10 +173,11 @@ function outOfTime() {
     setTimeout(newQuestion, 3000);
     $("#image-holder").show();
     displayLoseImage();
+    $("#reveal").show();
+    $("#reveal").text("The correct answer was " + questionArr[counter].answerCorrect);
 }
 
-
-// Function to hide start button and show the timer and first question with answers
+// Function to hide start button and show the timer and first question/answers upon click of the start button
 $("#start").click(function(){
     $("#start").hide();
     $("#game-over").hide();
@@ -203,19 +199,19 @@ $("#start").click(function(){
     $("#answer4").text(questionArr[counter].answersAll[3]);
 });
 
-
 // Function to compare the user's guess to the correct answer
 function compareAnswers() {
     $(".answer-choice").on("click", function(event){
         console.log(event);
         console.log("Event: " + event.target.textContent);
         console.log("Event Type: " + typeof(event.target.textContent));
-        // Testing to see if we have a correct answer
+        // If the user guessed correctly, the celebratory function is called and their score is updated
         if (event.target.textContent === questionArr[counter].answerCorrect) {
             console.log("MATCH");
             congrats();
             wins++;
         }
+        // If the user guessed incorrectly, the answer page functions is called and their score is updated
         else if (event.target.textContent !== questionArr[counter].answerCorrect) {
             answerPage();
             losses++
@@ -223,50 +219,43 @@ function compareAnswers() {
     })
 }
 
+// Calling the function to compare answers (above)
 compareAnswers();
  
-
+// Function to display the next question
 function newQuestion() {
-
-if (counter < questionArr.length - 1) {
-
-    counter++;
-    console.log("New question: " + questionArr[counter].question);
-    $("#question").show();
-    $("#question").text(questionArr[counter].question);
-    $(".hidebutton").show();
-    $("#answer1").text(questionArr[counter].answersAll[0]);
-    $("#answer2").text(questionArr[counter].answersAll[1]);
-    $("#answer3").text(questionArr[counter].answersAll[2]);
-    $("#answer4").text(questionArr[counter].answersAll[3]);
-    $("#yay").hide();
-    // timeLeft = 10;
-    startTimer();
-    $("#image-holder").hide();
-}
-else {
-    $("#yay").hide();
-    $("#timer").hide();
-    showStats();
-    $("#game-over").show();
-    $("#game-over").text("All done! Here's how you did:");
-    $("#image-holder").html("<img src=assets/images/end2.gif width='400px'>")
-    // setTimeout(newQuestion, 8000);
-    $("#start").show();
-    $("#start").text("START OVER");
-    counter = 0;
-    winImageCount = 0;
-    loseImageCount = 0;
-}};
-
-// Calling functions to start the game
+    // If the counter variable is less than the questions array (minus 1, to account for the 0th index), a new question is displayed and the timer restarts
+    if (counter < questionArr.length - 1) {
+        counter++;
+        console.log("New question: " + questionArr[counter].question);
+        $("#question").show();
+        $("#question").text(questionArr[counter].question);
+        $(".hidebutton").show();
+        $("#answer1").text(questionArr[counter].answersAll[0]);
+        $("#answer2").text(questionArr[counter].answersAll[1]);
+        $("#answer3").text(questionArr[counter].answersAll[2]);
+        $("#answer4").text(questionArr[counter].answersAll[3]);
+        $("#yay").hide();
+        $("#reveal").hide();
+        startTimer();
+        $("#image-holder").hide();
+    }
+    // Otherwise, the game ends, stats are displayed, and the start button becomes a "start over" button
+    else {
+        $("#yay").hide();
+        $("#reveal").hide();
+        $("#timer").hide();
+        showStats();
+        $("#game-over").show();
+        $("#game-over").text("All done! Here's how you did:");
+        $("#image-holder").html("<img src=assets/images/end2.gif width='400px'>")
+        $("#start").show();
+        $("#start").text("START OVER"); // If the start over button is pressed, all stats and counters reset to 0 and the game starts over
+        counter = 0;
+        winImageCount = 0;
+        loseImageCount = 0;
+    }};
 
 
-
-// NOTES/PSEUDOCODE:
-
-// 1. Use a setTimeout for changing from showing the correct answer (if the timer runs down) to showing the next question
-// 2. Otherwise, the click of an answer will trigger the post-answer page (and then a setTimeout will trigger the next question)
-// 3. Do I need to loop through the questions/answers, or should they just be referenced in if/else statements as questions[0], questions[1], etc.?
 
 });
